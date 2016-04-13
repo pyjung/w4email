115dfb199b524e3981e6f0931d15c72b46031243
pyjung <jungsmisc@gmail.com>
2016-04-13 14:09:06 +0000

    3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'mailgun'
+
 class HomeController < ApplicationController
     def begin
         
@@ -10,6 +12,20 @@ class HomeController < ApplicationController
         @sender = params[:sender]
         @title = params[:title]
         @content = params[:content]
+        
+        mg_client = Mailgun::Client.new("key-9cdeb8b738762cd026cc10523b5600dc")
+
+        message_params =  {
+                           from: @sender,
+                           to:   @recipient,
+                           subject: @title,
+                           text:    @content
+                          }
+        
+        result = mg_client.send_message('sandboxa746f5fbd5014767ac138b88bb85b2f6.mailgun.org', message_params).to_h!
+        
+        message_id = result['id']
+        message = result['message']
         
     end
     
